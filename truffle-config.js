2401ae1ce7eeef059daf73bea26d7c982b2e47a3
bitofcoding <92008241+bitofcoding@users.noreply.github.com>
2023-04-16 13:28:03 +0530

    Polygon Mumbai Testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = "drop prosper buffalo evolve biology review obey damage owner birth nothing mobile";
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +9,13 @@ module.exports = {
       port: 7545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
     },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+      }
   },
   
   contracts_directory: "./src/contracts",
